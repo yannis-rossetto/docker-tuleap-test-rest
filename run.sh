7d1c39391290711da8bce3a7212c7a83000294a1
Manuel Vacelet <manuel.vacelet@enalean.com>
2016-03-04 18:12:41 +0100

    Delegate to Tuleap part

--- run.sh
+++ run.sh
@@ -34,48 +34,16 @@ setup_tuleap() {
 	> /etc/tuleap/conf/local.inc
 }
 
-feed_database() {
-    mysql<<EOF
-GRANT ALL PRIVILEGES on *.* to 'tuleapadm'@'localhost' identified by 'welcome0' WITH GRANT OPTION; 
-CREATE DATABASE tuleap DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-FLUSH PRIVILEGES;
-EOF
-    mysql -u tuleapadm -pwelcome0 tuleap < /usr/share/tuleap/src/db/mysql/database_structure.sql
-    mysql -u tuleapadm -pwelcome0 tuleap<<EOF
-GRANT SELECT ON tuleap.user to dbauthuser@'localhost' identified by 'welcome0';
-GRANT SELECT ON tuleap.groups to dbauthuser@'localhost';
-GRANT SELECT ON tuleap.user_group to dbauthuser@'localhost';
-FLUSH PRIVILEGES;
-EOF
-    mysql -u tuleapadm -pwelcome0 tuleap < /usr/share/tuleap/src/db/mysql/database_initvalues.sql
-    mysql -u tuleapadm -pwelcome0 tuleap < /usr/share/tuleap/plugins/tracker/db/install.sql
-    mysql -u tuleapadm -pwelcome0 tuleap < /usr/share/tuleap/plugins/graphontrackersv5/db/install.sql
-    mysql -u tuleapadm -pwelcome0 tuleap < /usr/share/tuleap/plugins/agiledashboard/db/install.sql
-    mysql -u tuleapadm -pwelcome0 tuleap < /usr/share/tuleap/plugins/cardwall/db/install.sql
-}
-
-import() {
-    /usr/share/tuleap/src/utils/php-launcher.sh /usr/share/tuleap/src/utils/import_project_xml.php -u admin -i /usr/share/tuleap/tests/rest/init/base -m /usr/share/tuleap/tests/rest/init/base/users.csv
-    user_pwd=$(php /usr/share/tuleap/tools/utils/password_hasher.php -p "welcome0")
-    mysql -u tuleapadm -pwelcome0 tuleap -e "UPDATE user SET password='$user_pwd';"
-}
-
-#alias php="php -q -d date.timezone=Europe/Paris -d include_path=/usr/share/php:/usr/share/pear:/usr/share/tuleap/src/www/include:/usr/share/tuleap/src:/usr/share/jpgraph:. -d memory_limit=256M -d display_errors=On"
-
-
 setup_apache
+setup_tuleap
 
 service mysqld start
 service httpd start
 
-time feed_database
-
-setup_tuleap
-
-time import
+/usr/share/tuleap/tests/rest/init/bootstrap.sh
 
 cd /usr/share/tuleap/tests/rest
 # composer install
-vendor/bin/phpunit StupidTest.php
+echo vendor/bin/phpunit StupidTest.php
 
 exec bash
